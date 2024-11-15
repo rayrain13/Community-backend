94c9ee2019cb83477bc506779387570e155891de
rayrain13 <dirtin3@outlook.com>
2024-11-15 20:18:38 +0800

    feat:物业报修相关接口

---
+++ Desktop/团队作业/Community-backend/src/main/java/com/community/kairos/service/IRepairService.java
@@ -0,0 +1,37 @@
+package com.community.kairos.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.writingcode.www.community.entity.po.Repair;
+import com.writingcode.www.community.entity.vo.RepairVo;
+
+/**
+ *
+ * @author Chavy
+ * @date  2020/05/02
+ */
+public interface IRepairService extends IService<Repair> {
+
+    /**
+     * 添加保修单
+     * @param repair 保修单
+     * @return boolean
+     */
+    boolean addRepair(Repair repair);
+
+    /**
+     * 更新保修单
+     * @param repair 保修单
+     * @return boolean
+     */
+    boolean updateRepair(Repair repair);
+
+    /**
+     * 分页查询保修单
+     * @param userId 用户id
+     * @param page 分页bean
+     * @return Page<RepairVo>
+     */
+    Page<RepairVo> getRepair(Long userId, Page<RepairVo> page);
+
+}
