9074f532603fcb638f2172fec550be53e13b819e
rayrain13 <dirtin3@outlook.com>
2024-11-15 20:17:00 +0800

    feat:实体类entity

---
+++ Desktop/团队作业/Community-backend/src/main/java/com/community/kairos/entity/po/Car.java
@@ -0,0 +1,61 @@
+package com.community.kairos.entity.po;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Chavy
+ * @date  2020/05/02
+ */
+@Data
+@Accessors(chain = true)
+@TableName("car")
+public class Car implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public static final String ID = "id";
+    public static final String USER_ID = "user_id";
+    public static final String PLATE_NUMBER = "plate_number";
+    public static final String BRAND = "brand";
+    public static final String MODEL = "model";
+    public static final String COLOR = "color";
+
+    /**
+     * 车主用户id
+     */
+    private Long userId;
+
+    /**
+     * 主键id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 车牌号
+     */
+    private String plateNumber;
+
+    /**
+     * 品牌
+     */
+    private String brand;
+
+    /**
+     * 型号
+     */
+    private String model;
+
+    /**
+     * 颜色
+     */
+    private String color;
+
+}
---
+++ Desktop/团队作业/Community-backend/src/main/java/com/community/kairos/entity/vo/UserDetailVo.java
@@ -0,0 +1,21 @@
+package com.community.kairos.entity.vo;
+
+import com.writingcode.www.community.entity.po.Car;
+import com.writingcode.www.community.entity.po.House;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * @author Chavy
+ * @date 2020/5/5 22:30
+ */
+@Data
+@Accessors(chain = true)
+public class UserDetailVo {
+
+    private House houseInfo;
+
+    private Car carInfo;
+
+    private UserVo userInfo;
+}
