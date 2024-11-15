4b8f5f14140e337e1ae3e85ec75f0f9b9446a7c7
rayrain13 <dirtin3@outlook.com>
2024-11-15 20:23:32 +0800

    feat: 管理员管理用户相关接口

---
+++ Desktop/团队作业/Community-backend/src/main/java/com/community/kairos/dao/RolePermissionMapper.java
@@ -0,0 +1,18 @@
+package com.community.kairos.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.writingcode.www.community.entity.po.RolePermission;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author jobob
+ * @since 2020-05-02
+ */
+@Mapper
+public interface RolePermissionMapper extends BaseMapper<RolePermission> {
+
+}
