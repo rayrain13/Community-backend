f7f9264053046c9ffb8f14d1c683dbcbb6e2335f
rayrain13 <dirtin3@outlook.com>
2024-11-17 12:04:58 +0800

    feat:config

---
+++ Desktop/团队作业/Community-backend/src/main/java/com/community/kairos/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.community.kairos.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Chavy
+ * @date 2020/5/6
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+
+    /**
+     *   mybatis-plus分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setDialectType("mysql");
+        return page;
+    }
+}
